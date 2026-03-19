2309402f7de0e66b9403b772d08a1a735bc72585
dhtltmemos-cal <dhtlt.memos@gmail.com>
2026-03-19 11:08:23 +0700

    Khởi

---
+++ src/ToolWindows/SampleToolWindowControl.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace AsyncToolWindowSample.ToolWindows
+{
+    public partial class SampleToolWindowControl : UserControl
+    {
+        private SampleToolWindowState _state;
+
+        public SampleToolWindowControl(SampleToolWindowState state)
+        {
+            _state = state;
+
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string version = _state.DTE.FullName;
+
+            MessageBox.Show($"Visual Studio is located here: '{version}'");
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string version = _state.DTE.FullName;
+
+            MessageBox.Show($"Visual Studio is located here: '{version}'");
+        }
+    }
+}
